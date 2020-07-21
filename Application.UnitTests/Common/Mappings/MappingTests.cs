7b5f544d5c6d990ceefe836a26f71745885a2779
Morten Syhler <Morten.syhler@gmail.com>
2020-07-21 21:40:06 +0200

    added mapping unit tests

---
+++ Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using AutoMapper;
+using CoolWebsite.Application.Common.Mapping;
+using CoolWebsite.Application.DatabaseAccess.Financials.FinancialProjects.Queries.GetFinancialProjects.Models;
+using CoolWebsite.Domain.Entities.Financial;
+using CoolWebsite.Domain.Entities.Identity;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DTOMappingProfile>();
+
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Test]
+        [TestCase(typeof(FinancialProject), typeof(FinancialProjectDto))]
+        [TestCase(typeof(Receipt), typeof(ReceiptDto))]
+        [TestCase(typeof(ReceiptItem), typeof(ReceiptItemDto))]
+        [TestCase(typeof(ApplicationUser), typeof(UserDto))]
+
+        public void Handle_SupportMapping_ShouldMapFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = Activator.CreateInstance(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+    }
+}
--- CoolWebsite/Areas/Financial/Models/CreateReceiptModel.cs
+++ CoolWebsite/Areas/Financial/Models/CreateReceiptModel.cs
@@ -7,15 +7,7 @@ namespace CoolWebsite.Areas.Financial.Models
 {
     public class CreateReceiptModel
     {
-        //public string Location { get; set; }
-        
-        //public string Note { get; set; }
-        
-        //public DateTime DateVisited { get; set; }
-
         public string FinancialProjectId { get; set; }
-
-        //public List<ReceiptItemDto> ReceiptItemModels { get; set; }
 
         public ReceiptDto ReceiptDto { get; set; }
         public CreateReceiptItemVm CreateReceiptItemVm { get; set; }
