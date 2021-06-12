87de11073ccd448de632a3547d8cf89deb3b550f
Morten Syhler <morten.syhler@gmail.com>
2021-06-12 23:45:18 +0200

    Missing files from init commit

---
+++ BlazorServerWithAuth/Pages/Admin/Login/LoginPage.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoolWebsite.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorServerWithAuth.Pages.Admin.Login
+{
+    public class LoginPage : PageModel
+    {
+        private readonly IIdentityService _identityService;
+        private readonly ICurrentUserService _currentUserService;
+
+        public LoginPage(IIdentityService identityService, ICurrentUserService currentUserService)
+        {
+            _identityService = identityService;
+            _currentUserService = currentUserService;
+        }
+
+
+        public IActionResult OnGet()
+        {
+            
+            if (User == null) return Page();
+            if (User.Identity == null) return Page();
+            if (User.Identity.IsAuthenticated) return LocalRedirect("/admin");
+
+            return Page();
+        }
+
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        
+        public async Task<IActionResult> OnPostLogin(string username, string password)
+        {
+  
+            var result = await _identityService.LoginUser(username, password, false);
+
+            if (result.Succeeded) return LocalRedirect("/admin");
+
+            return Page();
+
+        }
+
+     
+    }
+}
---
+++ BlazorServerWithAuth/Services/CurrentUserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using CoolWebsite.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace BlazorServerWithAuth.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            UserId = httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            UserClaims = httpContextAccessor.HttpContext?.User;
+        }
+        
+        public string? UserId { get; set; }
+        
+        public ClaimsPrincipal? UserClaims { get; set; }
+    }
+}
